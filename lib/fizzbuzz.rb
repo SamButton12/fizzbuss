92e4124bbcab387e6f937a54b42da77c78ee0445
Sam Button <sam_button_94@hotmail.co.uk>
2022-04-12 17:38:56 +0100

    Co-authored-by: George Dainton <GeorgeDainton@users.noreply.github.com>

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,13 @@
 def fizzbuzz(number)
-  'fizz'
+  (1..number).map do |number|
+    if number % 3 == 0 && number % 5 != 0
+      "fizz"
+    elsif number % 5 == 0 && number % 3 != 0
+      "buzz"
+    elsif number % 3 == 0 && number % 5 == 0
+      "fizzbuzz"
+    else
+      number
+    end
+  end
 end
